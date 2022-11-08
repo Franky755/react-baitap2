17f0f50227777aa586181a8b0952d7e8af0cc42a
Franky755 <mittoknight@gmail.com>
2022-11-08 16:43:14 +0700

    update-ui

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,11 @@
+import '../maincss/input.css';
 
 const Input = (props) => {
   return (
-    <div>
-      <span>{props.icon}</span>
-      <input type={props.type} />
+    <div className='input-container'>
+      <span className='icon'>{props.icon}</span>
+      <label className='label'>{props.label}</label>
+      <input type={props.type} placeholder={props.placeholder} value={props.value} onChange={props.onChange} required={props.required} />
     </div>
   );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,19 +1,64 @@
 import { useState } from "react";
-import Input from "./components/Input";
+import Input from "../components/Input";
+import login from '../maincss/login.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value)
+  }
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleOnSubmit = (event) => {
+    if (email.length === 0 ) {
+      alert("Please input your email!")
+    } else {
+
+    }
+  }
+
 
   return (
-    <div className="form">
-      <div className="left-content">
-        <img src=''></img>
-      </div>
-      <div className="right-content">
-        USER LOGIN
-        <Input value={email} type="text" icon="" label="" onChange={() => {}} />
-        <Input value={password} type="password" icon="" label="" onChange={() => {}} />
+    <div className="container">
+      <div className="form">
+        <div className="left-content">
+          <img className='avatar' src="http://media.vov.vn/sites/default/files/styles/large/public/2022-08/anh-nen-avatar-dep_652403.jpg"
+            alt="avatar" ></img>
+        </div>
+        <div className="right-content">
+          <h2>User Login</h2>
+          <br />
+          <Input
+          value={email || ''}
+          type="text"
+          icon={<FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>}
+          label="Email: "
+          placeholder="Type your email"
+          onChange={handleChangeEmail}
+          required />
+
+          <Input
+          value={password}
+          type="password"
+          icon={<FontAwesomeIcon icon={faLock}></FontAwesomeIcon>}
+          label="Password: "
+          placeholder="Your password"
+          onChange={handleChangePassword}
+          required />
+
+          <br />
+          <button className="login" onClick={handleOnSubmit}>Login</button>
+          <br/>
+          <a href="">Forgot Username/</a> <a href=""> Password?</a>
+        </div>
       </div>
     </div>
   );
